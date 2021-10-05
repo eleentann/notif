9b76a95652d026c782e828031e7c3d7eaaf74764
eleentann <eleentanx@gmail.com>
2021-10-05 10:19:31 +0800

    Initial Commit

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-from kivy.uix.screenmanager import ScreenManager
-from kivymd.app import MDApp
-from kivy.lang import Builder
-from kivy.core.window import Window
+from kivy.app import App
+from kivy.uix.button import Button
 
-from kivymd.uix.label import MDLabel
-from kivymd.uix.button import MDFlatButton
-import json
-import requests
+class MainApp(App):
+  def build(self):
+    return Button(text="Hello World")
 
-Window.size = (310,500)
-
-class Slope(MDApp):
-    def build(self):
-        self.screen_manager = ScreenManager()
-        self.screen_manager.add_widget(Builder.load_file("main.kv"))
-        return self.screen_manager
-
-if __name__ == "__main__":
-    Slope().run()
+MainApp().run()
